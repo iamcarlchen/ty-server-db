316db68089408660522e2cf068089896cd3e3071
陈征 <carl.chen@chinaredstar.com>
2018-03-20 16:27:03 +0800

    非结构化数据管理器

---
+++ src/main/java/com/greatbee/core/db/rest/RestAPIManager.java
@@ -0,0 +1,22 @@
+package com.greatbee.core.db.rest;
+
+import com.greatbee.core.bean.oi.Field;
+import com.greatbee.core.bean.oi.OI;
+import com.greatbee.core.db.UnstructuredDataManager;
+
+import java.util.List;
+
+/**
+ * Rest API Manager
+ * <p/>
+ * Author: CarlChen
+ * Date: 2018/3/20
+ */
+public class RestAPIManager implements UnstructuredDataManager {
+
+    @Override
+    public String connect(OI oi, List<Field> fields) {
+        //TODO
+        return null;
+    }
+}
